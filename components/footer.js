b708114bdf5f6070ea890d41b47a5dfb9100e095
wuchenhao <wuchenhao@021.com>
2022-02-11 22:49:15 +0800

    :sparkles: edit  style

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="w-full z-10"> 
       <hr className="mb-8 border-gray-400 border-1 border-opacity-30" />
       <div className="mx-auto max-w-2xl text-indigo-800 dark:text-blue-300">
-        <div className="grid justify-between items-center w-full grid-cols-4 pb-8 sm:grid-cols-[repeat(4, min-content)]">
+        <div className="grid justify-between items-center w-full grid-cols-4 pb-2 sm:grid-cols-[repeat(4, min-content)]">
           <div className="footer-items">
             <Link href="/">
               <a>Home</a>
@@ -34,8 +34,18 @@ export default function Footer() {
       </div>
     
       <div className="w-full py-4 text-xs text-center">
-        <p>© 2022 - made with <span className="duration-700 cursor-pointer saturate-0 hover:saturate-100 hover:brightness-100 brightness-50 dark:brightness-[320%] dark:hover:brightness-100 hover:text-red-500 transition-color">
-         ♥ ︎</span>Cody Wu</p>
+        <p>
+          © 2022 - made with
+          <span className="px-1 duration-700 cursor-pointer 
+          saturate-0 hover:saturate-100 hover:brightness-100 brightness-50 dark:brightness-[320%] dark:hover:brightness-100 hover:text-red-500 transition-color">
+          ♥︎</span>
+          Cody Wu ・ Powered by
+          <Link to="route" href="#" rel="noopener noreferrer">
+            <a target="_blank" href="https://nextjs.org/">
+              <span className="px-1 text-blue-500 hover:cursor-pointer">Next JS.</span>
+            </a>
+          </Link>
+        </p>
       </div>
 
       <Scrolltotop />
